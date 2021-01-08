d0542cc85b195b9a5a36998f607681e7c65c0f28
james <jguest777@gmail.com>
2021-01-08 09:24:05 -0500

    added webp img format with success & enabled devOptimization

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const withOptimizedImages = require('next-optimized-images');
 
 module.exports = withOptimizedImages({
   /* config for next-optimized-images */
+  optimizeImagesInDev: true,
   // your config for other plugins or the general next.js here...
 });
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image src="/logo.png" width={1600} height={550} alt="test logo" />
         <img
-          src={require('../public/logo.png')}
+          src={require('../public/logo.png?webp')}
           alt="test logo two"
           style={{ width: '1600px', height: '550px' }}
         />
